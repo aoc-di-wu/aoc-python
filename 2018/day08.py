19063be9dce9c34043e2c9573ef7c972d1a07c55
Quint Daenen <quint.daenen@student.kuleuven.be>
2018-12-08 09:03:42 +0100

    2018, day eight (part 2)

--- 2018/day08.py
+++ 2018/day08.py
@@ -4,13 +4,23 @@ tree = [int(x) for x in open("inputs/day08.input").read().split()]
 def node(branch):
     children, metadata = branch[:2]
     branch = branch[2:]
-    sum_meta = 0
+    sum_meta, values = 0, []
     for x in range(children):
-        meta, branch = node(branch)
+        meta, branch, v = node(branch)
         sum_meta += meta
+        values.append(v)
     sum_meta += sum(branch[:metadata])
-    return sum_meta, branch[metadata:]
+
+    if children == 0:
+        return sum_meta, branch[metadata:], sum(branch[:metadata])
+    else:
+        v = 0
+        for x in branch[:metadata]:
+            if 0 < x <= len(values):
+                v += values[x - 1]
+        return sum_meta, branch[metadata:], v
 
 
-total, left = node(tree)
+total, left, value = node(tree)
 print("1: %d" % total)
+print("2: %d" % value)
